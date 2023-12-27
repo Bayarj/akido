ccaa13784dbdc279206bcc89c31aeb26f5292cb6
Bayarjargalaa <111161962+Bayarjargalaa@users.noreply.github.com>
2023-12-27 14:25:09 +0800

    signup

--- example/urls.py
+++ example/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name="home"),
     path('login', views.login, name="home"),
     path('signup', views.signup, name="home"),
 ]
--- example/views.py
+++ example/views.py
@@ -4,17 +4,9 @@ from datetime import datetime
 # from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-
+def index(request):
+    return render(request, 'index.html')
 def login(request):
-    # now = datetime.now()
-    # html = f'''
-    # <html>
-    #     <body>
-    #         <h1>Сайн уу хонгор минь</h1>
-    #         <p>The current time is { now }.</p>
-    #     </body>
-    # </html>
-    # '''
     return render(request, 'login.html')
 def signup(request):
     return render(request, 'signup.html')
