aacec3c4f106f39c66bb721e32a280605ab45305
Bayarjargal <jargal.otgon@yahoo.com>
2023-12-28 13:36:59 +0800

    statci

--- example/urls.py
+++ example/urls.py
@@ -1,11 +1,11 @@
 # example/urls.py
 from django.urls import path
-
 from . import views
 
 
+
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('login', views.login, name="home"),
-    path('signup', views.signup, name="home"),
+    path('', views.index, name="index"),
+    path('login', views.login, name="login"),
+    path('signup', views.signup, name="signup"),
 ]
