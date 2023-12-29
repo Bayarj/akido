5f6ad5c16a17ca4773f1c6043b8321a44b166d59
Bayarjargal <jargal.otgon@yahoo.com>
2023-12-29 12:34:27 +0800

    css

--- example/urls.py
+++ example/urls.py
@@ -6,6 +6,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login', views.login, name="login"),
-    path('signup', views.signup, name="signup"),
+    path('trainer.html', views.trainer, name="trainer"),
+    path('signup.html', views.signup, name="signup"),
+    path('why.html', views.why, name="why"),
+    path('contact.html', views.contact, name="signup"),
 ]
--- example/views.py
+++ example/views.py
@@ -6,7 +6,11 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
-def login(request):
-    return render(request, 'login.html')
+def trainer(request):
+    return render(request, 'trainer.html')
 def signup(request):
     return render(request, 'signup.html')
+def why(request):
+    return render(request, 'why.html')
+def contact(request):
+    return render(request, 'contact.html')
